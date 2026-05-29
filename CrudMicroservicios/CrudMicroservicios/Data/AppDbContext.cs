fdaeb652385aa3f77676349ddf6f4c484b29d78c
SATHEGAMERHACKS <santiospinaarias@gmail.com>
2026-05-28 20:22:12 -0500

    taller pendiente

---
+++ CrudMicroservicios/CrudMicroservicios/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CrudMicroservicios.Models;
+
+namespace CrudMicroservicios.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
---
+++ CrudMicroservicios/CrudMicroservicios/models/Producto.cs
@@ -0,0 +1,13 @@
+﻿namespace CrudMicroservicios.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+        public int Stock { get; set; }
+        public DateTime FechaCreacion { get; set; } = DateTime.Now;
+        public bool Activo { get; set; } = true;
+    }
+}
